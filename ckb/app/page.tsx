5281f00c7876050fff392957480e5b9e4547445d
yanguoyu <841185308@qq.com>
2024-11-28 18:12:15 +0800

    feat: Add source code link

--- ckb/app/page.tsx
+++ ckb/app/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import { XpubkeyForm } from "./xpubkey";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] flex flex-col gap-4 p-8 items-center">
-      <XpubkeyForm />
-    </div>
+    <>
+      <div className="bg-slate-100 w-full text-right px-8 py-2">
+        <Link href="https://github.com/yanguoyu/web3-tools/tree/main/ckb">
+          Source Code
+        </Link>
+      </div>
+      <div className="font-[family-name:var(--font-geist-sans)] flex flex-col gap-4 p-8 items-center">
+        <XpubkeyForm />
+      </div>
+    </>
   );
 }
